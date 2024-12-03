ba3575a12c0553bbe9d0d45a1d4db6b905769af0
DKAvocad00 <kurilodaniilds@gmail.com>
2024-12-03 15:40:20 +0300

    Changes in login: -Add login page; -Add login router; -Add fron for login page. Changes in register: -Change collor of background and button

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -58,12 +58,42 @@ router.post("/register", upload.single("profileImage"), async (req, res) => {
     /* Send a successful message*/
     res
       .status(200)
-      .json({ message: "User registered successfully", user: newUser });
+      .json({ message: "User registered successfully!", user: newUser });
   } catch (err) {
     console.log(err);
     res
       .status(500)
       .json({ message: "Registration failed!", error: err.message });
+  }
+});
+
+/*USER LOGIN */
+
+router.post("/login", async (req, res) => {
+  try {
+    /*Take the information from the form */
+    const { email, password } = req.body;
+
+    /* Check if user exists */
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(409).json({ message: "User doesn't exist!" });
+    }
+
+    /*Compose the password with the hashed password */
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials!" });
+    }
+
+    /*Generate JWN token */
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+
+    res.status(200).json({ token, user });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
   }
 });
 
